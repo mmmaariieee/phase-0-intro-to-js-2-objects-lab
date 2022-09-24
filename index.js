30910b32c332d6fd3ec012ee2ba710f3017ef74e
Mariia <m.goncharenkoo@icloud.com>
2022-09-24 01:23:43 -0400

    commit tree 4c49068163de671f5e19ecf58bb23902968a8e10

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mariia",
+    streetAddress: "8810 Flower Ave"
+}
+
+function updateEmployeeWithKeyAndValue(Object, key, value) {
+    let copyOfEmployee = {
+        ...employee,
+        [key]: value
+    }
+    return copyOfEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copyOfEmployee = {
+        ...employee,
+    }
+    delete copyOfEmployee[key];
+    return copyOfEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
